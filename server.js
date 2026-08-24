cd71a22c9364108f652d1aed2983768bca7505e2
w6109201 <yasser.naccar1@gmail.com>
2026-08-24 23:52:54 +0300

    إضافة ملفات المشروع بالكامل وتحديث نظام الشات والربط المحلي

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname)));
 // عدّل كلمة المرور إذا عندك password لـ root
 // ==========================================
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "127.0.0.1",
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
